f530ae681107a3737e18013939122b378b48efa5
baileytorch <baileytorch@hotmail.com>
2026-05-29 15:48:27 -0400

    Actualizando validación

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -11,11 +11,40 @@ window.onload = function(){
 function validarFormulario(){
     let nombre = document.getElementById('inputNombre')
     let rut = document.getElementById('inputRut')
+    let correo = document.getElementById('inputEmail')
+    let celular = document.getElementById('inputCelular')
+    let fechaNac = document.getElementById('inputNacimiento')
+    let nacionalidad = document.getElementById('selectNacionalidad')
+    let contrasena = document.getElementById('inputContrasena')
+    let repContrasena = document.getElementById('inputRepetirContrasena')
+    let direccion = document.getElementById('inputDireccion')
+    let foto = document.getElementById('inputFoto')
+    let genero = document.querySelector('input[name="radioGenero"]:checked')
+    let formularioValido = true
     
-    if(nombre.value == ''){
-        nombre.classList.add('alerta','is-invalid')
+    if(validarIpnut(nombre)){formularioValido = true}else{formularioValido=false}
+    if(validarIpnut(rut)){formularioValido = true}else{formularioValido=false}
+    if(validarIpnut(correo)){formularioValido = true}else{formularioValido=false}
+    if(validarIpnut(celular)){formularioValido = true}else{formularioValido=false}
+    if(validarIpnut(fechaNac)){formularioValido = true}else{formularioValido=false}
+    if(validarIpnut(nacionalidad)){formularioValido = true}else{formularioValido=false}
+    if(validarIpnut(contrasena)){formularioValido = true}else{formularioValido=false}
+    if(validarIpnut(repContrasena)){formularioValido = true}else{formularioValido=false}
+    if(validarIpnut(direccion)){formularioValido = true}else{formularioValido=false}
+    if(validarIpnut(foto)){formularioValido = true}else{formularioValido=false}
+
+    if(formularioValido){
+        alert('Datos ingresados correctamente, enviando datos...')
+    }
+}
+
+function validarIpnut(elemento){
+    if(elemento.value == ''){
+        elemento.classList.add('alerta','is-invalid')
+        return false;
     }else{
-        nombre.classList.remove('alerta','is-invalid')
-        nombre.classList.add('correcto','is-valid')
+        elemento.classList.remove('alerta','is-invalid')
+        elemento.classList.add('correcto','is-valid')
+        return true;
     }
 }
